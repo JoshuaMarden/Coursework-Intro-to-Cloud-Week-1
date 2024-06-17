ee5b10a9e037c8386cb4b9b237be8bec814a326c
Joshua Jack Marden <joshuasigma@Joshuas-MBP.lan>
2024-06-17 14:11:15 +0100

    complete Mondays challenge

--- coding_problems/01-monday/monday.py
+++ coding_problems/01-monday/monday.py
@@ -1,2 +1,19 @@
 def wave(people: str) -> list[str]:
-    pass
+
+    wave_list = []
+
+    people = list(people)
+
+    for index, char in enumerate(people):
+        if char == " ":
+            continue
+
+        people[index] = people[index].upper()
+        wave_list.append("".join(people))
+        people[index] = people[index].lower()
+
+    return wave_list
+
+
+if __name__ == "__main__":
+    print(wave('hello'))
