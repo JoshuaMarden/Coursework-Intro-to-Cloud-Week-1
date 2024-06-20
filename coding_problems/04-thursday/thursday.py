524e5e131be6135066190cbae1196bed2a90f5e0
Joshua Jack Marden <joshuasigma@Joshuas-MBP.lan>
2024-06-20 14:03:43 +0100

    complete Thurdays challenge

--- coding_problems/04-thursday/thursday.py
+++ coding_problems/04-thursday/thursday.py
@@ -1,2 +1,19 @@
 def pyramid(n: int) -> list:
-    pass
+
+    filled_array = []
+    counter = 0
+
+    for _ in range(n):
+        counter += 1
+        list_to_insert = [1 for _ in range(counter)]
+        filled_array.append(list_to_insert)
+        print(list_to_insert)
+
+    return filled_array
+
+
+if __name__ == "__main__":
+    # print(pyramid(0))
+    # print(pyramid(1))
+    # print(pyramid(2))
+    print(pyramid(3))
