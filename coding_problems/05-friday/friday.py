1fca3a093e71614950809f1576404e3902191dee
Joshua Jack Marden <joshuasigma@Joshuas-MBP.lan>
2024-06-20 15:40:25 +0100

    update

--- coding_problems/05-friday/friday.py
+++ coding_problems/05-friday/friday.py
@@ -1,2 +1,21 @@
 def is_valid_IP(strng: str) -> bool:
-    pass
+
+    fragments = strng.split(".")
+
+    if not len(fragments) == 4:
+        return False
+
+    for octet in fragments:
+        if not 0 < int(octet) < 256:
+            return False
+
+    return True
+
+
+if __name__ == "__main__":
+
+    print(is_valid_IP('1.2.3.4'))
+    print(is_valid_IP('123.45.67.89'))
+
+    print(is_valid_IP('1.2.3'))
+    print(is_valid_IP('123.456.78.90'))
