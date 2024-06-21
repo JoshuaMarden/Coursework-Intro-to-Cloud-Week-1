1dcbd9a3cc77a39c09320ffc3da71dc73c30a14d
Joshua Jack Marden <joshuasigma@Joshuas-MBP.lan>
2024-06-21 13:20:49 +0100

    Complete Fridays challenge

--- coding_problems/05-friday/friday.py
+++ coding_problems/05-friday/friday.py
@@ -1,12 +1,20 @@
 def is_valid_IP(strng: str) -> bool:
 
+    for char in strng:
+        if not char in ["1", '2', '3', '4', '5', '6', '7', '8', '9', '0', '.']:
+            return False
+
     fragments = strng.split(".")
 
     if not len(fragments) == 4:
         return False
 
     for octet in fragments:
-        if not 0 < int(octet) < 256:
+        if octet == "":
+            return False
+        if not 0 <= int(octet) < 256:
+            return False
+        if octet[0] == '0' and len(octet) > 1:
             return False
 
     return True
@@ -14,8 +22,9 @@ def is_valid_IP(strng: str) -> bool:
 
 if __name__ == "__main__":
 
-    print(is_valid_IP('1.2.3.4'))
-    print(is_valid_IP('123.45.67.89'))
+    # print(is_valid_IP('1.2.3.4'))
+    # print(is_valid_IP('123.45.67.89'))
 
-    print(is_valid_IP('1.2.3'))
-    print(is_valid_IP('123.456.78.90'))
+    # print(is_valid_IP('1.2.3'))
+    # print(is_valid_IP('123.456.78.90'))
+    print(is_valid_IP('127.1.1.0'))
